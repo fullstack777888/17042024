771092577592d5cb995bbc9fb44bdeb1c6d50deb
fullstack777888 <166302097+fullstack777888@users.noreply.github.com>
2024-04-17 21:23:38 +0300

    Add files via upload

---
+++ 17042024_part_three/app.js
@@ -0,0 +1,27 @@
+const persons = [{
+    "name": "Dani",
+    "lastName": "Conor",
+    "age": 30
+},
+{
+    name: "Beni",
+    "lastName": "Conor",
+    "age": 30
+},
+{
+    "name": "Sarah",
+    "lastName": "Conor",
+    "age": 30
+}]
+
+// Not valid Json
+const myObject = {
+    asd : "dwqdwq",
+    sayHello : ()=>{
+        console.log('hello')
+    }
+}
+
+const json = {
+    "asd" : "dwqdwq"
+}
