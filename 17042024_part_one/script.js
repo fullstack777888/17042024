26e79399fc322383123e313350b2a53bb09d8f03
fullstack777888 <166302097+fullstack777888@users.noreply.github.com>
2024-04-17 20:37:25 +0300

    Add files via upload

---
+++ 17042024_part_one/script.js
@@ -0,0 +1,22 @@
+
+// const button = document.getElementById('myBtn');
+
+// button.addEventListener('click', () => {
+//     const myP = document.getElementById('myP');
+//     if (myP.style.color === '' || myP.style.color === 'black') {
+//         myP.style.color = 'red'
+//     } else {
+//         myP.style.color = 'black'
+//     }
+// })
+
+
+// const elements = document.getElementsByClassName('para');
+
+// for (const element of elements) {    
+//     element.style.color = 'red';
+// }
+
+// document.getElementById('myP').innerHTML = '<button id="dog">dog</button>'
+
+// document.getElementById('myP').innerText = 'Dog'
